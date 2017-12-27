f247c97abc8f7e27d553ae29783791e2812e5bdb
Travis Robson <travis.robson@ligo.org>
2017-12-27 16:05:39 -0500

    .

---
+++ Ecc_IO.h
@@ -0,0 +1,20 @@
+#ifndef Ecc_IO_h
+#define Ecc_IO_h
+
+struct Data
+{
+	long N, NFFT;
+	long left_pad;
+	long under_samp;
+	
+	double dt, df;
+	double sr, Ny;
+
+};
+void read_soln_no_RR(double *time_series, FILE *fptr, struct Data *data);
+
+void read_soln(double *time_series, FILE *fptr, struct Data *data);
+void print_dft(double *time_series, FILE *fptr, struct Data *data);
+void print_spa(double *time_series, FILE *fptr, struct Data *data);
+
+#endif /* Ecc_IO */
